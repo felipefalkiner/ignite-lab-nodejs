982be717786380015a056268a594096d1b5a5600
Felipe Magosso Poveda <felipe.magosso@gmail.com>
2023-01-25 17:59:02 -0300

    created the routes for the api and added the insominia collection to the project

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,10 +3,22 @@ import { DatabaseModule } from '../database/database.modules';
 
 import { SendNotification } from '@application/use-cases/send-notification';
 import { NotificationsController } from './controllers/notifications.controller';
+import { CancelNotification } from '@application/use-cases/cancel-Notification';
+import { CountRecipientNotifications } from '@application/use-cases/count-recipient-notifications';
+import { GetRecipientNotifications } from '@application/use-cases/get-recipient-notifications';
+import { ReadNotification } from '@application/use-cases/read-Notification';
+import { UnreadNotification } from '@application/use-cases/unread-Notification';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    ReadNotification,
+    UnreadNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+  ],
 })
 export class HttpModule {}
